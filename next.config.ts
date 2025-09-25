47e36f85e787b49712365a473a5a93c7ddf3a430
Kyle Klus <kyle.klus.2@gmail.com>
2025-09-25 14:23:26 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: '',
+  basePath: '/text-tools',
   output: 'export',
   images: { unoptimized: true, qualities: [100] },
   outputFileTracingRoot: __dirname,
   env: {
-    basePath: '',
+    basePath: '/text-tools',
   },
 };
 
