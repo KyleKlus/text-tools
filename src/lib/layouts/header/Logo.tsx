1eeab6e482bf49c3dbd6adfb86cf0b978bbc447b
Kyle Klus <kyle.klus@f-i.de>
2025-10-09 14:56:12 +0200

    FIX: Yet another basepath fix

--- src/lib/layouts/header/Logo.tsx
+++ src/lib/layouts/header/Logo.tsx
@@ -3,16 +3,11 @@
 import { basePath } from '@/app/layout';
 import styles from './Logo.module.css';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 export default function Logo() {
-  const router = useRouter();
   return (
-    <div
-      onClick={(e) => {
-        e.preventDefault();
-        router.push('https://kyleklus.de/');
-      }}
+    <a
+      href="https://kyleklus.de/"
       className={[styles.logoWrapper].join(' ')}
     >
       <Image
@@ -24,6 +19,6 @@ export default function Logo() {
         className={[].join(' ')}
       ></Image>
       <p>Kyle Klus</p>
-    </div>
+    </a>
   );
 }
